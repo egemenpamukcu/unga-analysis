7fa8432d32260c88e08e74faffc782d984c8a57c
egemenpamukcu <72174414+egemenpamukcu@users.noreply.github.com>
2021-04-12 22:02:25 -0500

    corpus added

---
+++ generate_corpus.py
@@ -0,0 +1,48 @@
+import requests
+import io
+import re
+import pandas as pd
+import numpy as np
+import json
+import shutil
+from mpi4py import MPI
+import math
+import pdftotext
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+name = MPI.Get_processor_name()
+
+def create_corpus(pdf_urls):
+    #pdf_urls = [[k, v] for k, v in all_pdfs.items()]
+    for i, pdf_url in enumerate(pdf_urls):
+        req = requests.get(pdf_url[1])
+        with io.BytesIO(req.content) as f:
+            try:
+                pdf = pdftotext.PDF(f)
+            except pdftotext.Error:
+                print(pdf_url[0], 'ERRORED OUT')
+                continue
+        text = ''
+        for page in pdf:
+            text += page
+        pdf_url.append(text.strip())
+        print('rank {}:'.format(rank), str((round((i + 1)/len(pdf_urls)*100, 4))) + '%')
+    return pd.DataFrame(pdf_urls, columns=['Resolution', 'url', 'Text'])
+
+
+with open('all_pdfs.txt') as pdfs:
+    all_pdfs = json.load(pdfs)
+
+pdf_urls = [[k, v] for k, v in all_pdfs.items()]
+
+n = math.ceil(len(pdf_urls) / size)
+partitions = [pdf_urls[i * n:(i + 1) * n] for i in range((len(pdf_urls) + n - 1) // n )]
+
+for i in range(size):
+    if rank == i:
+        corpus = create_corpus(partitions[i])
+        corpus.to_json('raw_corpus_{}.json'.format(i))
+
+
