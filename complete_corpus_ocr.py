093d8015e8b28d8a9cb4209172ce7e1c04fe7c52
egemenpamukcu <72174414+egemenpamukcu@users.noreply.github.com>
2021-04-13 21:39:15 -0500

    error handling

--- complete_corpus_ocr.py
+++ complete_corpus_ocr.py
@@ -1,4 +1,3 @@
-import pdftotext
 import requests
 import io
 import pandas as pd
@@ -15,7 +14,7 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
-raw_all = pd.read_json('raw_resolutions.json')
+raw_all = pd.read_json('raw_resolutions.json').sample(n=8)
 n = math.ceil(len(raw_all) / 8)
 partitions = [raw_all[i * n:(i + 1) * n] for i in range((len(raw_all) + n - 1) // n )]
 
@@ -23,15 +22,27 @@ for i in range(size):
 	if rank == i:
 		raw = partitions[i]
 		x = 0
+		errors = []
 		for ind, k in raw.iterrows():
-		    if k['Text'] == '':
-		        url = k['url']
-		        req = requests.get(url)
-		        pages = pdf2image.convert_from_bytes(req.content)
-		        res = ''
-		        for page in pages:
-		            res += pytesseract.image_to_string(page)
-		        k['Text'] = res
-		    x += 1
-		    print('rank:', rank, ' status:', x/len(raw))
+			x += 1
+			if k['Text'] == '':
+				print('rank:', rank, ' status:', x/len(raw), k['Resolution'])
+				for t in range(2):
+					try:
+						url = k['url']
+						req = requests.get(url)
+						pages = pdf2image.convert_from_bytes(req.content)
+						res = ''
+						for page in pages:
+							res += pytesseract.image_to_string(page)
+						k['Text'] = res
+						break
+					except:
+						if t == 1:
+							errors.append(k['Resolution'])
+						continue
+
+		if len(errors) >= 1:
+			with open('ocr_errors_{}.txt'.format(i), 'w') as err:
+				json.dump(errors, err)
 		raw.to_json('complete_corpus_raw_{}.json'.format(i))
